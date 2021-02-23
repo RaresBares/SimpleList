6140736369d399ae1303066d8d921221c4254973
Rares <rsahleanu@gmail.com>
2021-02-23 23:52:42 +0100

    Initial commit

---
+++ Main.pas
@@ -0,0 +1,56 @@
+(*
+ * Project: untitled32
+ * User: Rares
+ * Date: 23.02.2021
+ *)
+program Main;
+type
+  listenRefElement = ^listenElement;
+  listenElement = record
+  next:listenRefElement;
+    value:Real;
+    name:string;
+  before:listenRefElement;
+  end;
+  
+  var
+    ListenBegin:listenRefElement;
+
+
+
+Procedure display;
+var
+  tmp:listenRefElement;
+begin
+tmp := ListenBegin;
+  while tmp <> nil do
+  begin
+    WriteLn('Zahl: ',(tmp^.value));
+    tmp := tmp^.next;
+  end;
+end;
+  
+  Procedure add;
+  
+  var
+    akt:listenRefElement;
+    
+  
+  begin
+    New(akt);
+    akt^.next := nil;
+    akt^.value := 2;
+    ListenBegin^.next := akt;
+  end;
+  
+begin
+  new(ListenBegin);
+  ListenBegin^.value := 1;
+  ListenBegin^.name := 'abc';
+  ListenBegin^.before := nil;
+  
+  add();
+//  (ListenBegin^.next)^.before := ListenBegin;
+  display()
+end.
+
